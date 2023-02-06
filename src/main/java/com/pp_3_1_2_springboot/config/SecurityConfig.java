45d6489754ae6948d61b22ecb7473085aa6035f3
Westalex777 <westalex777@yandex.ru>
2023-02-07 02:11:18 +0600

    commit 3

--- src/main/java/com/pp_3_1_2_springboot/config/SecurityConfig.java
+++ src/main/java/com/pp_3_1_2_springboot/config/SecurityConfig.java
@@ -26,7 +26,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-//                .antMatchers("/**").permitAll()
                 .antMatchers("/user").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated()

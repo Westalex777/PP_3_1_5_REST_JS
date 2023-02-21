eb11a69d591f44ec6eda05ef1905911d1f38761f
Westalex777 <westalex777@yandex.ru>
2023-02-21 13:55:30 +0600

    commit 21/02

--- src/main/java/com/pp_3_1_5/controller/AuthRestApiController.java
+++ src/main/java/com/pp_3_1_5/controller/AuthRestApiController.java
@@ -2,6 +2,8 @@ package com.pp_3_1_5.controller;
 
 import com.pp_3_1_5.dto.UserDto;
 import com.pp_3_1_5.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthRestApiController {
 
     @GetMapping("auth")
-    public UserDto userAuth(@AuthenticationPrincipal User userAuth) {
-        return new UserDto(userAuth);
+    public ResponseEntity<UserDto> userAuth(@AuthenticationPrincipal User userAuth) {
+        return new ResponseEntity<>(new UserDto(userAuth), HttpStatus.OK);
     }
 }
